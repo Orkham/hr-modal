c744a20af69018445af6fb114def31722010a2a2
Nicolas Chatelais <ni.chatelais@gmail.com>
2022-01-13 08:35:04 +0100

    comments

--- src/modal.js
+++ src/modal.js
@@ -3,6 +3,11 @@ import { createPortal } from 'react-dom'
 import React, { useState } from 'react'
 import './modal.css'
 
+
+/**
+ * Création d'un hook personnalisé pour la gestion des données de la fenêtre modale au sein de l'application dans laquelle elle sera déployée
+ * @returns {isShowing : constante gérant l'affichage ou non de la modale ; toggle : appel modifiant l'option d'affichage}
+ */
 export const useModal = () => {
   const [isShowing, setIsShowing] = useState(false)
 
@@ -16,6 +21,12 @@ export const useModal = () => {
   }
 }
 
+
+/**
+ * 
+ * @param {*} objet regroupant les méthodes d'affichage de la modale 
+ * @returns Composant représentant la fenêtre modale à afficher dans l'application
+ */
 const Modal = ({ isShowing, hide }) =>
   isShowing
     ? createPortal(
